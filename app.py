40759ec70e28902828fcad301162a17035ebde40
PotatoShaga <stronguy991@gmail.com>
2024-11-09 17:42:52 -0500

    finished exponential and log

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+from scipy.interpolate import make_interp_spline #actually maybe lines not curving isnt a problem
 
-text = st.text_input(label="To use these services, you need to provide your SIN and address here:")
-
-if st.button(label="show text"): #if button is pressed, its true
-    st.write(text)
+#Questions: is state session/saving needed for fully marks? how many equations are needed? is this graph enough info?
+#and just local host is fine?
 
 #st.radio for picking fit
 "Input values manually or attach csv"
@@ -22,7 +22,7 @@ with cols[0]: #1st column, empty_df
         'Positive integer values only. 10 data points created.'
         empty_df_size = 9 
     empty_df_size = int(empty_df_size) + 1
-    init_dict = {x:0 for x in range(1,empty_df_size)}
+    init_dict = {x:0.0 for x in range(1,empty_df_size)}
     empty_df = pd.DataFrame(init_dict, index=["x","y"]) #initialising empty df
     empty_df = empty_df.transpose()
     empty_df["x"] = empty_df.index #sets x column to index so that polyfit doesnt die immediately with empty df
@@ -57,13 +57,16 @@ with cols[2]: #3rd column, df to be fit
 fit_selection = st.radio(label="Choose your fit:", options=["Linear", "Polynomial", "Exponential", "Logarithmic"])
 
 if fit_selection == "Linear":
-    trendline = np.polyfit(df["x"], df["y"], deg=1)
-    trendline_y_values = np.polyval(trendline, df["x"])
+    trendline = np.polyfit(df["x"], df["y"], deg=1) #generates coefficients for linear function, m and b
+    trendline_y_values = np.polyval(trendline, df["x"]) #calculates y values with given coefficients
     plt.scatter(df["x"], df["y"])
     plt.xlabel("x")
     plt.ylabel("y")
     plt.plot(df["x"],trendline_y_values)
     st.pyplot(plt)
+
+    equation = f"{round(trendline[0], 5)}x + {round(trendline[1], 5)}"
+    equation
 
 elif fit_selection == "Polynomial":
     degree = st.slider("Degree:", min_value = 2, max_value = 10, value=2)
@@ -75,3 +78,46 @@ elif fit_selection == "Polynomial":
     plt.ylabel("y")
     plt.plot(df["x"],trendline_y_values)
     st.pyplot(plt)
+    
+    trendline = [round(trendline[i], 5) for i,x in enumerate(trendline)]
+    equation = "y = "
+    for x in range(degree+1, 0, -1):
+        if x == 1: #just makes the last string added not have a trailing +
+            equation += f"({list(reversed(trendline))[x-1]})x^{x-1}"
+        else:
+            equation += f"({list(reversed(trendline))[x-1]})x^{x-1} + "
+    equation
+
+elif fit_selection == "Exponential":
+    def exponential(x, A, b, c):
+        y = A * np.exp(b*x) + c #np.exp is a function that takes b,x as input
+        return y
+    coeffs = curve_fit(exponential, xdata=df["x"], ydata=df["y"], p0=(1,1,1))
+
+    trendline_y_values = coeffs[0][0] * np.exp(coeffs[0][1] * df["x"]) + coeffs[0][2]
+    plt.scatter(df["x"], df["y"])
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.plot(df["x"],trendline_y_values)
+
+    st.pyplot(plt)
+
+    equation = f"y = {round(coeffs[0][0], 5)} * e^({round(coeffs[0][1], 5)}x) + ({round(coeffs[0][2], 5)})"
+    equation
+
+elif fit_selection == "Logarithmic":
+    def logarithmic(x, A, b):
+        y = A * np.log(x) + b #np.exp is a function that takes b,x as input
+        return y
+    coeffs = curve_fit(logarithmic, xdata=df["x"], ydata=df["y"])
+
+    trendline_y_values = coeffs[0][0] * np.log(df["x"]) + coeffs[0][1]
+    plt.scatter(df["x"], df["y"])
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.plot(df["x"],trendline_y_values)
+    st.pyplot(plt)
+
+    equation = f"y = {round(coeffs[0][0], 5)} * log(x) + ({round(coeffs[0][1], 5)})"
+    equation
+
